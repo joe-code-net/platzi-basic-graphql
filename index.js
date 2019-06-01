63f7006a591c1f18f5b53b5747a440f1ac1910c8
joe-code-net <joe.code.netgmail.com>
2019-06-01 13:56:02 -0600

    Queries y Resolvers

--- index.js
+++ index.js
@@ -6,10 +6,26 @@ const { graphql, buildSchema } = require('graphql')
 const schema = buildSchema(`
     type Query {
         hello: String
+        saludo: String
     }
 `)
 
+//Configurar los resolvers
+const resolvers = {
+    hello: () => {
+        return 'Hola Mundo'
+    },
+    saludo: () => {
+        return 'Hola a todos'
+    }
+}
+
 //ejecutar el query hello
-graphql(schema, '{ hello }').then((data) => {
+graphql(schema, '{ hello }', resolvers).then((data) => {
+    console.log(data)
+})
+
+//ejecutar el query hello
+graphql(schema, '{ saludo }', resolvers).then((data) => {
     console.log(data)
 })
