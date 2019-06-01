05a0c29165cd1fdec176cae4f1b3537bff1f2e38
joe-code-net <joe.code.netgmail.com>
2019-06-01 13:33:00 -0600

    schemas and types

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const { graphql, buildSchema } = require('graphql')
+
+// definiendo el esquema
+const schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+`)
+
+//ejecutar el query hello
+graphql(schema, '{ hello }').then((data) => {
+    console.log(data)
+})
